05afddcceeec4adce673c2e894eb8ba844edb699
student <student@AQ2-0673>
2024-05-13 17:06:17 +0900

    ファーストコミット

---
+++ spring-KeitoMATSUOKA_7135/src/main/java/com/example/demo/SpringKeitoMatsuoka7135Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKeitoMatsuoka7135Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKeitoMatsuoka7135Application.class, args);
+	}
+
+}
---
+++ spring-KeitoMATSUOKA_7135/src/test/java/com/example/demo/SpringKeitoMatsuoka7135ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKeitoMatsuoka7135ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
